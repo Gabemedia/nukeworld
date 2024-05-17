ff6929fa07bad4e579aaa62c445b5d2434fddd7d
Gabemedia <info@gabemedia.net>
2024-05-17 18:39:08 +0200

    VUE STORE TEST

--- nukeworld/src/store/index.js
+++ nukeworld/src/store/index.js
@@ -16,176 +16,165 @@ const state = reactive({
     inventory: [],
   },
   quests: reactive(JSON.parse(localStorage.getItem('quests')) || defaultQuests),
-  items: items,
+  items,
 });
 
-const store = createStore({
-  state,
-  mutations: {
-    addCharacter(state, character) {
-      state.characters.push(character);
-    },
-    updateCharacter(state, character) {
-      Object.assign(state.character, character);
-    },
-    addExp(state, exp) {
-      state.character.exp += exp;
-    },
-    increaseMaxExp(state, amount) {
-      state.character.maxExp += amount;
-    },
-    addMoney(state, amount) {
-      state.character.money += amount;
-    },
-    updateCharacterInArray(state, character) {
-      const characterInArray = state.characters.find(ch => ch.email === character.email);
-      if (characterInArray) {
-        Object.assign(characterInArray, character);
-      }
-    },
-    increaseCharacterLevel(state) {
-      state.character.level += 1;
-    },
-    increaseCharacterLevelInArray(state, character) {
-      const characterInArray = state.characters.find(ch => ch.email === character.email);
-      if (characterInArray) {
-        characterInArray.level += 1;
-      }
-    },
-    updateQuestState(state, { quest, newState }) {
-      const questIndex = state.quests.findIndex(q => q.name === quest.name);
-      if (questIndex !== -1) {
-        state.quests[questIndex] = { ...state.quests[questIndex], ...newState };
-      }
-    },
-    startQuest(state, quest) {
-      const index = state.quests.findIndex(q => q.name === quest.name);
-      if (index !== -1) {
-        state.quests[index] = { ...quest, disabled: true, state: 'in-progress', progress: 0, startTime: Date.now() };
-      }
-    },
-    completeQuest(state, quest) {
-      const index = state.quests.findIndex(q => q.name === quest.name);
-      if (index !== -1) {
-        state.quests[index] = { ...quest, state: 'completed' };
-      }
-    },
-    resetQuest(state, quest) {
-      const index = state.quests.findIndex(q => q.name === quest.name);
-      if (index !== -1) {
-        state.quests[index] = { ...quest, disabled: false, state: 'not-started', progress: 0 };
-      }
-    },
-    setQuests(state, quests) {
-      state.quests = quests;
-    },
-    updateQuestProgress(state, { questIndex, progress, remainingTime }) {
-      state.quests[questIndex].progress = progress;
-      state.quests[questIndex].remainingTime = remainingTime;
-    },
+const getters = {
+  characterInArray: (state) => (email) => state.characters.find((ch) => ch.email === email),
+};
+
+const mutations = {
+  addCharacter(state, character) {
+    state.characters.push(character);
   },
-  actions: {
-    login({ commit }, { username, email, password }) {
-      commit('updateCharacter', { name: username, email, password });
-    },
-    createCharacter({ commit, state }) {
-      const newCharacter = { ...state.character, level: 1, exp: 1, maxExp: 2500, money: 0 };
-      commit('addCharacter', newCharacter);
-      commit('updateCharacter', newCharacter);
-    },
-    updateCharacter({ commit }, character) {
-      commit('updateCharacter', character);
-    },
-    increaseExp({ commit, state, dispatch }, amount) {
-      commit('addExp', amount);
-      if (state.character.exp >= state.character.maxExp) {
-        dispatch('levelUp');
-      }
-    },
-    decreaseExp({ commit, state }) {
-      if (state.character.exp - 5 >= 0) {
-        commit('addExp', -5);
-      }
-    },
-    increaseMoney({ commit, state }, amount) {
-      commit('addMoney', amount);
-      commit('updateCharacterInArray', state.character);
-    },
-    decreaseMoney({ commit, state }, amount) {
-      if (state.character.money - amount >= 0) {
-        commit('addMoney', -amount);
-        commit('updateCharacterInArray', state.character);
-      }
-    },
-    levelUp({ commit, state }) {
-      const overflowExp = state.character.exp - state.character.maxExp;
-      const newMaxExp = Math.floor(state.character.maxExp * 1.6);
-      commit('updateCharacter', { exp: overflowExp * 2, maxExp: newMaxExp });
-      commit('increaseCharacterLevel');
-      commit('increaseCharacterLevelInArray', state.character);
-    },
-    startQuestProgress({ commit, state }, quest) {
-      const startTime = quest.startTime || Date.now();
-      const questIndex = state.quests.findIndex(q => q.name === quest.name);
-      state.quests[questIndex].startTime = startTime;
+  updateCharacter(state, character) {
+    Object.assign(state.character, character);
+  },
+  updateCharacterInArray(state, character) {
+    const characterInArray = getters.characterInArray(state)(character.email);
+    if (characterInArray) {
+      Object.assign(characterInArray, character);
+    }
+  },
+  increaseCharacterLevel(state) {
+    state.character.level += 1;
+  },
+  increaseCharacterLevelInArray(state, character) {
+    const characterInArray = getters.characterInArray(state)(character.email);
+    if (characterInArray) {
+      characterInArray.level += 1;
+    }
+  },
+  updateQuestState(state, { quest, newState }) {
+    const questIndex = state.quests.findIndex((q) => q.name === quest.name);
+    if (questIndex !== -1) {
+      state.quests[questIndex] = { ...state.quests[questIndex], ...newState };
+    }
+  },
+  startQuest(state, quest) {
+    const index = state.quests.findIndex((q) => q.name === quest.name);
+    if (index !== -1) {
+      state.quests[index] = { ...quest, disabled: true, state: 'in-progress', progress: 0, startTime: Date.now() };
+    }
+  },
+  completeQuest(state, quest) {
+    const index = state.quests.findIndex((q) => q.name === quest.name);
+    if (index !== -1) {
+      state.quests[index] = { ...quest, state: 'completed' };
+    }
+  },
+  resetQuest(state, quest) {
+    const index = state.quests.findIndex((q) => q.name === quest.name);
+    if (index !== -1) {
+      state.quests[index] = { ...quest, disabled: false, state: 'not-started', progress: 0 };
+    }
+  },
+  setQuests(state, quests) {
+    state.quests = quests;
+  },
+  updateQuestProgress(state, { questIndex, progress, remainingTime }) {
+    state.quests[questIndex].progress = progress;
+    state.quests[questIndex].remainingTime = remainingTime;
+  },
+};
 
-      const intervalId = setInterval(() => {
-        const elapsedTime = Date.now() - startTime;
-        const progress = Math.min((elapsedTime / quest.duration) * 100, 100);
-        const remainingTime = Math.max(0, quest.duration - elapsedTime);
-        commit('updateQuestProgress', { questIndex, progress, remainingTime });
-        if (remainingTime <= 0) {
-          clearInterval(intervalId);
-          commit('completeQuest', quest);
-        }
-      }, 1000);
+const actions = {
+  login({ commit }, { username, email, password }) {
+    commit('updateCharacter', { name: username, email, password });
+  },
+  createCharacter({ commit, state }) {
+    const newCharacter = { ...state.character, level: 1, exp: 1, maxExp: 2500, money: 0 };
+    commit('addCharacter', newCharacter);
+    commit('updateCharacter', newCharacter);
+  },
+  updateCharacter({ commit }, character) {
+    commit('updateCharacter', character);
+    commit('updateCharacterInArray', character);
+  },
+  increaseExp({ commit, state, dispatch }, amount) {
+    commit('updateCharacter', { exp: state.character.exp + amount });
+    if (state.character.exp >= state.character.maxExp) {
+      dispatch('levelUp');
+    }
+  },
+  decreaseExp({ commit, state }) {
+    const newExp = Math.max(state.character.exp - 5, 0);
+    commit('updateCharacter', { exp: newExp });
+  },
+  increaseMoney({ commit, state }, amount) {
+    commit('updateCharacter', { money: state.character.money + amount });
+  },
+  decreaseMoney({ commit, state }, amount) {
+    const newMoney = Math.max(state.character.money - amount, 0);
+    commit('updateCharacter', { money: newMoney });
+  },
+  levelUp({ commit, state }) {
+    const overflowExp = state.character.exp - state.character.maxExp;
+    const newMaxExp = Math.floor(state.character.maxExp * 1.6);
+    commit('updateCharacter', { exp: overflowExp * 2, maxExp: newMaxExp, level: state.character.level + 1 });
+    commit('increaseCharacterLevelInArray', state.character);
+  },
+  startQuestProgress({ commit, state }, quest) {
+    const startTime = quest.startTime || Date.now();
+    const questIndex = state.quests.findIndex((q) => q.name === quest.name);
+    state.quests[questIndex].startTime = startTime;
 
+    const intervalId = setInterval(() => {
       const elapsedTime = Date.now() - startTime;
+      const progress = Math.min((elapsedTime / quest.duration) * 100, 100);
       const remainingTime = Math.max(0, quest.duration - elapsedTime);
+      commit('updateQuestProgress', { questIndex, progress, remainingTime });
       if (remainingTime <= 0) {
         clearInterval(intervalId);
         commit('completeQuest', quest);
       }
-    },
-    handleQuest({ dispatch, commit, state }, quest) {
-      if (quest.state === 'not-started') {
-        commit('startQuest', quest);
-        const startTime = Date.now();
-        state.quests[state.quests.findIndex(q => q.name === quest.name)].startTime = startTime;
-        dispatch('startQuestProgress', { ...quest, startTime });
-      } else if (quest.state === 'in-progress') {
-        dispatch('startQuestProgress', quest);
-      }
-    },
-    claimRewards({ commit, dispatch, state }, quest) {
-      dispatch('increaseExp', quest.exp);
-      dispatch('increaseMoney', quest.money);
-    
-      // Check if the quest has a reward
-      if (quest.reward && quest.reward.length > 0) {
-        // Loop through the reward item IDs
-        quest.reward.forEach(rewardId => {
-          // Find the item object in the items array from the state
-          const rewardItem = state.items.find(item => item.id === rewardId);
-    
-          if (rewardItem) {
-            // Add the reward item to the character's inventory
-            state.character.inventory.push(rewardItem);
-    
-            // Display a notification or message to the player
-            console.log(`You received a ${rewardItem.name} as a reward for completing the quest "${quest.name}"!`);
-          }
-        });
-      }
-    
-      commit('resetQuest', quest);
-      commit('updateCharacterInArray', state.character); // Add this line to update the character in the characters array
-    },
-      clearQuests({ commit }) {
-      commit('setQuests', []);
-      commit('setQuests', defaultQuests);
-    },
+    }, 1000);
+
+    const elapsedTime = Date.now() - startTime;
+    const remainingTime = Math.max(0, quest.duration - elapsedTime);
+    if (remainingTime <= 0) {
+      clearInterval(intervalId);
+      commit('completeQuest', quest);
+    }
   },
+  handleQuest({ dispatch, commit, state }, quest) {
+    if (quest.state === 'not-started') {
+      commit('startQuest', quest);
+      const startTime = Date.now();
+      state.quests[state.quests.findIndex((q) => q.name === quest.name)].startTime = startTime;
+      dispatch('startQuestProgress', { ...quest, startTime });
+    } else if (quest.state === 'in-progress') {
+      dispatch('startQuestProgress', quest);
+    }
+  },
+  claimRewards({ commit, dispatch, state }, quest) {
+    dispatch('increaseExp', quest.exp);
+    dispatch('increaseMoney', quest.money);
+
+    if (quest.reward && quest.reward.length > 0) {
+      quest.reward.forEach((rewardId) => {
+        const rewardItem = state.items.find((item) => item.id === rewardId);
+        if (rewardItem) {
+          state.character.inventory.push(rewardItem);
+          console.log(`You received a ${rewardItem.name} as a reward for completing the quest "${quest.name}"!`);
+        }
+      });
+    }
+
+    commit('resetQuest', quest);
+    commit('updateCharacterInArray', state.character);
+  },
+  clearQuests({ commit }) {
+    commit('setQuests', []);
+    commit('setQuests', defaultQuests);
+  },
+};
+
+const store = createStore({
+  state,
+  getters,
+  mutations,
+  actions,
 });
 
 watch(
