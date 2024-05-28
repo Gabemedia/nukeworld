c99d43ff64ade8bbc3c05fc6bf2d4986cbf1dfca
LokalGruppen <info@gabemedia.net>
2024-05-28 09:16:07 +0200

    Nukeworld

--- nukeworld/vue.config.js
+++ nukeworld/vue.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-transpileDependencies: [/node_modules[\\/]*/]
-}
+  transpileDependencies: [/node_modules[\\/]*/],
+  chainWebpack: config => {
+    const isProduction = process.env.NODE_ENV === 'production';
+
+    config.module
+      .rule('mbtiles')
+      .test(/\.mbtiles$/)
+      .use('file-loader')
+      .loader('file-loader')
+      .options({
+        name: isProduction ? 'assets/maps/[name].[hash:8].[ext]' : 'assets/maps/[name].[ext]',
+      });
+  },
+};
