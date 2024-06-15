ee2e63a5bf874989da2c31c0c8eb457c88a94d85
Gabemedia <info@gabemedia.net>
2024-06-15 14:58:36 +0200

    publicevent

---
+++ nukeworld/src/store/publicEvents.js
@@ -0,0 +1,21 @@
+export default [
+    {
+      id: 1,
+      name: 'Public Event 1',
+      desc: 'This is the description for Public Event 1.',
+      exp: 1000,
+      money: 100,
+      duration: 60000,
+      progress: 0,
+      state: 'not-started',
+      claimed: false,
+      disabled: false,
+      reward: [1],
+      rewardChance: 0.5,
+      armorReward: [1],
+      armorRewardChance: 0.2,
+      levelRequirement: 1,
+    },
+    // Add more public events as needed
+  ];
+  
