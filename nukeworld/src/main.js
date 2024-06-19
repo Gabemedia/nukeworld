ad861b6af074de038e7eb2b2445174300f4741c2
LokalGruppen <info@gabemedia.net>
2024-06-19 15:36:29 +0200

    Update main.js

--- nukeworld/src/main.js
+++ nukeworld/src/main.js
@@ -1,11 +1,9 @@
 import { createApp } from 'vue';
-import { ToolTip } from '@programic/vue3-tooltip';
+import 'bootstrap/dist/js/bootstrap.bundle';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import Toast from 'vue3-toastify';
-import 'bootstrap/dist/js/bootstrap.bundle';
-import '@programic/vue3-tooltip/dist/index.css';
 import 'vue3-toastify/dist/index.css';
 import 'leaflet/dist/leaflet.css';
 import { LMap, LTileLayer, LMarker } from '@vue-leaflet/vue-leaflet';
@@ -15,9 +13,9 @@ const app = createApp(App);
 app.component('l-map', LMap);
 app.component('l-tile-layer', LTileLayer);
 app.component('l-marker', LMarker);
-app.component('ToolTip', ToolTip);
 
 app.use(router);
 app.use(store);
 app.use(Toast);
+
 app.mount('#app');
