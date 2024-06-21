39ed94bb5da0ee85a33760801585a8dfd888bf6a
Gabemedia <info@gabemedia.net>
2024-06-22 00:56:40 +0200

    QuestDialog

--- nukeworld/src/components/data/controller/QuestDialog.vue
+++ nukeworld/src/components/data/controller/QuestDialog.vue
@@ -1,13 +1,27 @@
 <template>
-    <div class="dialog-system">
-      <div class="mb-1">
-        <h6 class="mb-3 text-uppercase fw-bold text-start">Dialog</h6>
+  <div class="dialog-system">
+    <div class="mb-1">
+      <h6 class="mb-3 text-uppercase fw-bold text-start">Dialog</h6>
+    </div>
+    <div class="dialog-log small">
+      <!-- NPC conversation box -->
+      <div class="conversation-box npc-box mb-4">
+        <img class="sidebar-icon" :src="require(`@/assets/interface/icons/encounter.png`)" title="NPC">
+        <div class="message-box npc-message">
+          smg is a powerful ranged weapon. It is used for short-range combat. smg is a powerful ranged weapon. It is used for short-range combat. smg is a powerful ranged weapon. It is used for short-range combat.
+        </div>
       </div>
-      <div class="dialog-log border border-1 small">
+      <!-- Player conversation box -->
+      <div class="conversation-box player-box">
+        <div class="message-box player-message">
+          smg is a powerful ranged weapon. It is used for short-range combat. smg is a powerful ranged weapon. It is used for short-range combat. smg is a powerful ranged weapon. It is used for short-range combat.
+        </div>
+        <img class="sidebar-icon" :src="require(`@/assets/interface/icons/encounter.png`)" title="Player">
       </div>
     </div>
-  </template>
-  
+  </div>
+</template>
+
   <script>
   import { mapState } from 'vuex';
   
@@ -34,20 +48,12 @@
   };
   </script>
   
-  <style scoped>
-  div.modal-body {
-    margin:0!important;
-    padding:0!important;
-  }
-  div.modal-content {
-    background-color: #f0f0f0!important;
-    margin:0!important;
-  }
+  <style lang="scss" scoped>
   .dialog-system {
-  }
-  .dialog-system .icon{
-    height: 20px;
-    width: auto;
+    border-radius: 5px;
+    color: white;
+    text-shadow: rgba(0, 0, 0, 1) 0px 0px 2px;
+    font-size: 0.8rem;
   }
   
   .dialog-log {
@@ -55,20 +61,41 @@
     overflow-y: auto;
   }
   
-  .dialog-log ul {
-    list-style-type: none;
-    padding: 0;
+  .conversation-box {
+    display: flex;
+    align-items: flex-start;
+    margin-bottom: 10px;
   }
   
-  .dialog-log li {
-    margin-bottom: 5px;
-    padding: 5px;
-    border-radius: 3px;
+  .npc-box {
+    flex-direction: row;
   }
   
-  .player-action {
-    background-color: #d4edda;
-    color: #155724;
+  .player-box .sidebar-icon{ 
+  -webkit-transform: scaleX(-1);
+  transform: scaleX(-1);
+}
+
+  
+  .sidebar-icon {
+    width: 35px;
+    height: 35px;
+    margin: 0 10px;
+  }
+  
+  .message-box {
+    background-color: rgba(255, 255, 255, 0.1);
+    border-radius: 5px;
+    padding: 10px;
+    flex-grow: 1;
+  }
+  
+  .npc-message {
+    margin-right: 15px;
+  }
+  
+  .player-message {
+    margin-left: 15px;
   }
   </style>
-  
+    
